e9d9374a5f8c71746bf042115cbd1154473059b9
Nikita Ourazbaev <41582891+nikitaourazbaev@users.noreply.github.com>
2025-06-30 16:57:34 -0400

    Focus plugin: Fix Chromium warning about aria-hidden="true"

--- packages/focus/src/index.js
+++ packages/focus/src/index.js
@@ -108,6 +108,8 @@ export default function (Alpine) {
                 fallbackFocus: () => el,
             }
 
+            let undoInert = () => {}
+
             if (modifiers.includes('noautofocus')) {
                 options.initialFocus = false
             } else {
@@ -116,9 +118,14 @@ export default function (Alpine) {
                 if (autofocusEl) options.initialFocus = autofocusEl
             }
 
+            if (modifiers.includes('inert')) {
+                options.onActivate = () => {
+                    undoInert = setInert(el);
+                }
+            }
+
             let trap = createFocusTrap(el, options)
 
-            let undoInert = () => {}
             let undoDisableScrolling = () => {}
 
             const releaseFocus = () => {
@@ -139,7 +146,6 @@ export default function (Alpine) {
                 // Start trapping.
                 if (value && ! oldValue) {
                     if (modifiers.includes('noscroll')) undoDisableScrolling = disableScrolling()
-                    if (modifiers.includes('inert')) undoInert = setInert(el)
 
                     // Activate the trap after a generous tick. (Needed to play nice with transitions...)
                     setTimeout(() => {
